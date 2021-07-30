e1e192f13ab97722d8cee6f744395878d8a1c8e7
ashwinitadkal <88185696+ashwinitadkal@users.noreply.github.com>
2021-07-30 15:05:15 +0530

    Update Demo1.java

--- Demo_git_project/src/Demo1.java
+++ Demo_git_project/src/Demo1.java
@@ -4,6 +4,11 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO");
-	}
+		
 
+}
+	public void m1()
+		{
+		System.out.println("GitHub");
+	}
 }
